3b516845fd4903c811314c4ff63a92dd083a812b
ahmedbechirmezhoud <79605129+ahmedbechirmezhoud@users.noreply.github.com>
2021-11-27 14:09:42 +0100

    improve constants according to experiments

--- settings.h
+++ settings.h
@@ -8,27 +8,28 @@
 //PID params initialization
 
 #define KI_LINEAR  0
-#define KP_LINEAR  1
-#define KD_LINEAR  1
+#define KP_LINEAR  1.75
+#define KD_LINEAR  0
 
 #define KI_ANGULAR 0
-#define KP_ANGULAR 1
-#define KD_ANGULAR 1
+#define KP_ANGULAR 0.8
+#define KD_ANGULAR 0
 
 // Motor max Velocity
-#define MAX_VELOCITY 1023
-#define MIN_VELOCITY 0
+#define MAX_VELOCITY 1024
+#define MIN_VELOCITY 300
+#define ACC_RATE 0.005
  
-#define WHEEL_PERIMETER  250  //ENCODER'S WHEEL 
+#define WHEEL_PERIMETER 250  //ENCODER'S WHEEL
 
-#define ENTRAXE 350
+#define ENTRAXE 362
 
 // Robot initialization
 #define X_INIT 0
 #define Y_INIT 0
 #define ALPHA_INIT 0
 
-#define PRECISION_LINEAR 0.1
-#define PRECISION_ANGULAR 0.1
+#define PRECISION_LINEAR 30
+#define PRECISION_ANGULAR 120
 
 #endif /* SRC_SETTINGS_H_ */
