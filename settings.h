755733b6feb5884f78fc1c53ce0be0f68edf2651
ahmedbechirmezhoud <79605129+ahmedbechirmezhoud@users.noreply.github.com>
2021-11-30 20:47:18 +0100

    fix odometry and relative angle functions

--- settings.h
+++ settings.h
@@ -8,28 +8,27 @@
 //PID params initialization
 
 #define KI_LINEAR  0
-#define KP_LINEAR  1.75
-#define KD_LINEAR  0
+#define KP_LINEAR  0.8
+#define KD_LINEAR  .1
 
 #define KI_ANGULAR 0
-#define KP_ANGULAR 0.8
-#define KD_ANGULAR 0
+#define KP_ANGULAR 0.2
+#define KD_ANGULAR .1
 
 // Motor max Velocity
 #define MAX_VELOCITY 1024
-#define MIN_VELOCITY 300
-#define ACC_RATE 0.005
+#define MIN_VELOCITY 380
  
 #define WHEEL_PERIMETER 250  //ENCODER'S WHEEL
 
-#define ENTRAXE 362
+#define ENTRAXE 360
 
 // Robot initialization
 #define X_INIT 0
 #define Y_INIT 0
 #define ALPHA_INIT 0
 
-#define PRECISION_LINEAR 30
-#define PRECISION_ANGULAR 120
+#define PRECISION_LINEAR 7
+#define PRECISION_ANGULAR 30
 
 #endif /* SRC_SETTINGS_H_ */
